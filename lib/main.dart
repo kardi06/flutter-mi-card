6aea8509d32edd4b278a1a183594a0563c60c960
kardi.ib <kardi.ib@gmail.com>
2025-05-18 15:50:31 +0700

    style(main) : center the content and add divider below flutter dev

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -35,6 +36,11 @@ class MyApp extends StatelessWidget {
                     fontSize: 20.0,
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
               ),
               Container(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
